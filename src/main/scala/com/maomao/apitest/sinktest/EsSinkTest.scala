6b5ff59fbd2d79e51994ba63e0319f8cffd71b7e
337340028@qq.com <159357mnbMNB>
2019-12-13 23:14:00 +0800

    sink Test

---
+++ src/main/scala/com/maomao/apitest/sinktest/EsSinkTest.scala
@@ -0,0 +1,51 @@
+package com.maomao.apitest.sinktest
+
+import java.util
+
+import com.maomao.apitest.SensorReading
+import org.apache.flink.api.common.functions.RuntimeContext
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.connectors.elasticsearch.{ElasticsearchSinkFunction, RequestIndexer}
+import org.apache.flink.streaming.connectors.elasticsearch6.ElasticsearchSink
+import org.apache.http.HttpHost
+import org.elasticsearch.client.Requests
+
+object EsSinkTest {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+
+    //source
+    val inputStream = env.readTextFile("/home/mao/study/flinkdemo/src/main/resources/test")
+    //1 、基本转换算子与简单聚合算子  transfrom
+    val dataStream = inputStream.map(data => {
+      val strings = data.split(",")
+      SensorReading(strings(0).trim, strings(1).trim.toLong, strings(2).trim.toDouble)
+    })
+
+    //sink
+    val httpHosts = new util.ArrayList[HttpHost]()
+    httpHosts.add(new HttpHost("localhost"), 9200)
+    //创建一个esSinkBuilder
+    val esSink = new ElasticsearchSink.Builder[SensorReading](httpHosts, new ElasticsearchSinkFunction[SensorReading] {
+      override def process(t: SensorReading, runtimeContext: RuntimeContext, requestIndexer: RequestIndexer): Unit = {
+        println("saving data" + t)
+
+        //包装成一个json或者Map
+        val json = new util.HashMap[String, String]()
+        json.put("id", t.id)
+        json.put("temperature", t.temperature.toString)
+        json.put("timestamp", t.timestamp.toString)
+        //创建index request 准备发送数据
+        val indexRequest = Requests.indexRequest().index("sensor").`type`("readingdata").source(json)
+        //利用indexr发送请求  写入数据
+        requestIndexer.add(indexRequest)
+        println("data save success")
+      }
+    }).build()
+    dataStream.addSink(esSink)
+
+    env.execute("EsSinkTest")
+  }
+
+}
--- src/main/scala/com/maomao/apitest/sinktest/KafkaSinkTest.scala
+++ src/main/scala/com/maomao/apitest/sinktest/KafkaSinkTest.scala
@@ -1,22 +1,36 @@
 package com.maomao.apitest.sinktest
 
+import java.util.Properties
+
 import com.maomao.apitest.SensorReading
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.streaming.api.scala._
-import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaProducer010, FlinkKafkaProducer011}
+import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaConsumer010, FlinkKafkaProducer010, FlinkKafkaProducer011}
 
 object KafkaSinkTest {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
-    val inputStream = env.readTextFile("/home/mao/study/flinkdemo/src/main/resources/test")
+    //    val inputStream = env.readTextFile("/home/mao/study/flinkdemo/src/main/resources/test")
+    val props = new Properties()
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put("group.id", "group1")
+    //    props.put("enable.auto.commit", "true")
+    //    props.put("auto.commit.interval.ms", "1000")
+    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put("auto.offset.reset", "latest")
+    val stream4 = env.addSource(new FlinkKafkaConsumer010[String]("input", new SimpleStringSchema(), props))
+
 
     //1 、基本转换算子与简单聚合算子
-    val dataStream = inputStream.map(data => {
+    val dataStream = stream4.map(data => {
       val strings = data.split(",")
       SensorReading(strings(0).trim, strings(1).trim.toLong, strings(2).trim.toDouble).toString
     })
-    dataStream.addSink(new FlinkKafkaProducer010[String]("localhost:9092","test",new SimpleStringSchema()))
+
+
+    dataStream.addSink(new FlinkKafkaProducer010[String]("localhost:9092", "output", new SimpleStringSchema()))
 
     env.execute("KafkaSinkTest")
   }
